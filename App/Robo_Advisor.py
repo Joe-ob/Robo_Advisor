9bc8afcc5ab5653c812e2a6a48b418132a1a0c3d
JoeObrien <jpo52@georgetown.edu>
2021-03-04 12:13:10 -0500

    Basic Requirements started
    Still need work organizing the data that is pulled from online but so far i can pull the data fine

--- App/Robo_Advisor.py
+++ App/Robo_Advisor.py
@@ -1,25 +1,27 @@
 # this is the "app/robo_advisor.py" file
-
-
 import os
 from dotenv import load_dotenv
+import requests
+import json
 
 load_dotenv()
-print(os.getenv("ALPHAVANTAGE_API_KEY"))
+ALPHAVANTAGE_API_KEY=os.getenv("ALPHAVANTAGE_API_KEY")
 
-#print("-------------------------")
-#print("SELECTED SYMBOL: XYZ")
-#print("-------------------------")
-#print("REQUESTING STOCK MARKET DATA...")
-#print("REQUEST AT: 2018-02-20 02:00pm")
-#print("-------------------------")
-#print("LATEST DAY: 2018-02-20")
-#print("LATEST CLOSE: $100,000.00")
-#print("RECENT HIGH: $101,000.00")
-#print("RECENT LOW: $99,000.00")
-#print("-------------------------")
-#print("RECOMMENDATION: BUY!")
-#print("RECOMMENDATION REASON: TODO")
-#print("-------------------------")
-#print("HAPPY INVESTING!")
-#print("-------------------------")
+
+stock_symbol=input("Please enter the cryptocurrency or stock ticker you are looking for:")
+print("Requesting Stock Market Data for", stock_symbol, "...")
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey={ALPHAVANTAGE_API_KEY}"
+response=requests.get(request_url)
+
+
+parsed_response = json.loads(response.text)
+#print(parsed_response["Time Series (Daily)"])
+
+print(parsed_response["Time Series (Daily)"]["2021-03-03"])
+
+for item{} in parsed_response["Time Series (Daily)"]:
+    print(item)
+    for data in item:
+        print(data)
+
