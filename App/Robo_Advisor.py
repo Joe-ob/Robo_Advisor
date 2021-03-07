eba094622dd65a89d9d31113761b82faa73973a2
JoeObrien <jpo52@georgetown.edu>
2021-03-07 14:20:03 -0500

    Import data
    Not much has changed but I made edits to my base while following along to the video walkthrough

--- App/Robo_Advisor.py
+++ App/Robo_Advisor.py
@@ -1,27 +1,43 @@
 # this is the "app/robo_advisor.py" file
-import os
-from dotenv import load_dotenv
+
 import requests
 import json
 
-load_dotenv()
-ALPHAVANTAGE_API_KEY=os.getenv("ALPHAVANTAGE_API_KEY")
 
-
-stock_symbol=input("Please enter the cryptocurrency or stock ticker you are looking for:")
-print("Requesting Stock Market Data for", stock_symbol, "...")
-
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey={ALPHAVANTAGE_API_KEY}"
+request_url = ("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo")
 response=requests.get(request_url)
+#print(type(response)) #<>lass 'requests.models.Response'>
+#print(response.status_code) #>200
+#print(response.text)
+parsed_response=json.loads(response.text)
 
 
-parsed_response = json.loads(response.text)
-#print(parsed_response["Time Series (Daily)"])
+breakpoint()
 
-print(parsed_response["Time Series (Daily)"]["2021-03-03"])
 
-for item{} in parsed_response["Time Series (Daily)"]:
-    print(item)
-    for data in item:
-        print(data)
+#import os
+#from dotenv import load_dotenv
+#import requests
+#import json
+#
+#load_dotenv()
+#ALPHAVANTAGE_API_KEY=os.getenv("ALPHAVANTAGE_API_KEY")
+#
+#
+#stock_symbol=input("Please enter the cryptocurrency or stock ticker you are looking for:")
+#print("Requesting Stock Market Data for", stock_symbol, "...")
+#
+#request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey={ALPHAVANTAGE_API_KEY}"
+#response=requests.get(request_url)
+#
+#
+#parsed_response = json.loads(response.text)
+##print(parsed_response["Time Series (Daily)"])
+#
+#print(parsed_response["Time Series (Daily)"]["2021-03-03"])
+#
+#for item in parsed_response["Time Series (Daily)"]:
+#    print(item)
+#    for data in item:
+#        print(data)
 
