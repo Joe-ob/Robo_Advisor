7eff657a62060576198b8120623ed2d10f5097ff
JoeObrien <jpo52@georgetown.edu>
2021-03-07 15:01:42 -0500

    Recent High
    Created a for loop to find the recent high price

--- App/Robo_Advisor.py
+++ App/Robo_Advisor.py
@@ -12,18 +12,24 @@ def to_usd(my_price):
 
 request_url = ("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo")
 response=requests.get(request_url)
-#print(type(response)) #<>lass 'requests.models.Response'>
-#print(response.status_code) #>200
-#print(response.text)
+
 parsed_response=json.loads(response.text)
-print(parsed_response.keys())
+
+tsd=parsed_response["Time Series (Daily)"]
+
+dates=list(tsd.keys()) #This assumes first day is on top but can sort later
+
+latest_day=dates[0]
 
 Last_refreshed=parsed_response["Meta Data"]["3. Last Refreshed"]
 
-print(Last_refreshed)
+latest_close=tsd[latest_day]["4. close"]
 
-latest_close=parsed_response["Time Series (Daily)"]["2021-03-05"]["4. close"]
-print(latest_close)
+high_prices=[]
+for day in dates:
+    daily_high=float(tsd[day]["2. high"])
+    high_prices.append(daily_high)
+recent_high=max(high_prices)
 
 
 
@@ -35,7 +41,7 @@ print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {Last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
