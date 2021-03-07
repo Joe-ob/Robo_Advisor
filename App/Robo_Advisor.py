85cd9053bac0898f65caf6889be07390d98087ac
JoeObrien <jpo52@georgetown.edu>
2021-03-07 15:34:46 -0500

    csv file
    For loop adds all of the day's data to the csv file and the data is formatted properly

--- App/Robo_Advisor.py
+++ App/Robo_Advisor.py
@@ -26,25 +26,34 @@ latest_close=tsd[latest_day]["4. close"]
 
 high_prices=[]
 low_prices=[]
+
 for day in dates:
     daily_high=float(tsd[day]["2. high"])
     high_prices.append(daily_high)
 
     daily_low=float(tsd[day]["3. low"])
     low_prices.append(daily_low)
+
 recent_high=max(high_prices)
 recent_low=min(low_prices)
 
 
 csv_file_path=os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
-with open(csv_file_path, "w") as csv_file:
-    writer=csv.DictWriter(csv_file, fieldnames=["city", "name"])
-    writer.writeheader()
-    writer.writerow({"city": "New York", "name": "Yankees"})
-    writer.writerow({"city": "New York", "name": "Mets"})
-    writer.writerow({"city": "Boston", "name": "Red Sox"})
-    writer.writerow({"city": "New Haven", "name": "Ravens"})
 
+csv_headers=["timestamp", "open", "high", "low", "close", "volume"]
+with open(csv_file_path, "w") as csv_file:
+    writer=csv.DictWriter(csv_file, fieldnames=csv_headers)
+    writer.writeheader()
+    for day in dates:
+
+        writer.writerow({
+            "timestamp": day,
+            "open": to_usd(float(tsd[day]["1. open"])),
+            "high": to_usd(float(tsd[day]["2. high"])),
+            "low": to_usd(float(tsd[day]["3. low"])),
+            "close": to_usd(float(tsd[day]["4. close"])),
+            "volume": tsd[day]["5. volume"]
+        })
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
