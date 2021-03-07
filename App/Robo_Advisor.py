2892de7b1f45420939e8eb7c943035c9d376f7e7
JoeObrien <jpo52@georgetown.edu>
2021-03-07 14:40:47 -0500

    Closing Price and Date
    Parsing through the data

--- App/Robo_Advisor.py
+++ App/Robo_Advisor.py
@@ -2,6 +2,12 @@
 
 import requests
 import json
+import datetime
+
+
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
+
 
 
 request_url = ("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo")
@@ -10,34 +16,33 @@ response=requests.get(request_url)
 #print(response.status_code) #>200
 #print(response.text)
 parsed_response=json.loads(response.text)
+print(parsed_response.keys())
+
+Last_refreshed=parsed_response["Meta Data"]["3. Last Refreshed"]
+
+print(Last_refreshed)
+
+latest_close=parsed_response["Time Series (Daily)"]["2021-03-05"]["4. close"]
+print(latest_close)
 
 
-breakpoint()
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: {Last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
 
 
-#import os
-#from dotenv import load_dotenv
-#import requests
-#import json
-#
-#load_dotenv()
-#ALPHAVANTAGE_API_KEY=os.getenv("ALPHAVANTAGE_API_KEY")
-#
-#
-#stock_symbol=input("Please enter the cryptocurrency or stock ticker you are looking for:")
-#print("Requesting Stock Market Data for", stock_symbol, "...")
-#
-#request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey={ALPHAVANTAGE_API_KEY}"
-#response=requests.get(request_url)
-#
-#
-#parsed_response = json.loads(response.text)
-##print(parsed_response["Time Series (Daily)"])
-#
-#print(parsed_response["Time Series (Daily)"]["2021-03-03"])
-#
-#for item in parsed_response["Time Series (Daily)"]:
-#    print(item)
-#    for data in item:
-#        print(data)
 
